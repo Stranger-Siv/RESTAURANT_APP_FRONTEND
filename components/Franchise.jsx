caf8cd21b81f38f9af339ad76c300cb59e9d5010
Sivram Pradhan <programmersiv21@gmail.com>
2024-08-26 21:30:29 +0530

    changes

---
+++ components/Franchise.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+const Franchise = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [date, setDate] = useState("");
+  const [place, setPlace] = useState("");
+  const [phone, setPhone] = useState("");
+  const navigate = useNavigate();
+
+  const handleFranchise = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/api/v1/franchise/send",
+        { firstName, lastName, email, phone, date, place },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      toast.success(data.message);
+      setFirstName("");
+      setLastName("");
+      setPhone(0);
+      setEmail("");
+      setPlace("");
+      setDate("");
+      navigate("/success");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  return (
+    <section className="reservation" id="franchise">
+      <div className="container">
+      <div className="banner">
+          <div className="reservation_form_box">
+            <h1>ASK FOR FRANCHISE</h1>
+            <p>For Further Questions, Please Call</p>
+            <form>
+              <div>
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Last Name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
+              <div>
+                <input
+                  type="date"
+                  placeholder="Date"
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                />
+                <input
+                  type="place"
+                  placeholder="Place"
+                  value={place}
+                  onChange={(e) => setPlace(e.target.value)}
+                />
+              </div>
+              <div>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="email_tag"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                  type="number"
+                  placeholder="Phone"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+              </div>
+              <button type="submit" onClick={handleFranchise}>
+                RESERVE NOW{" "}
+                <span>
+                  <HiOutlineArrowNarrowRight />
+                </span>
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="banner">
+          <img src="/franchise.png" alt="res" />
+        </div>
+        
+      </div>
+    </section>
+  );
+};
+
+export default Franchise;
